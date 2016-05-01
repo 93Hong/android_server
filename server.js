d667985d5f56fe2de2a89236b580bdbf5f24cacc
93Hong <sk2rldnr@gmail.com>
2016-05-02 00:41:39 +0900

    <test mongo>

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-var express = require('express');
+var net = require('net');
+var jsonSocket = require('json-socket');
+var mongoose = require('mongoose');
 
-var app = express();
-
-app.get('/', function(req, res) {
-  res.send('hello world');
+mongoose.connect("mongodb://mobile:mobile@ds019950.mlab.com:19950/mobileteam");
+var db = mongoose.connection;
+db.once("open", function() {
+  console.log("DB connected");
+});
+db.on("error", function(err) {
+  console.log("DB error : " + err);
 });
 
-app.listen(52273, function() {
+var userSchema = mongoose.Schema({
+  id: {type:String, required:true, unique:true},
+  password: {type:String, required:true},
+  role: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now}
+});
+var User = mongoose.model('user', userSchema);
+
+
+/////////////////////////////////////////////////////////////
+
+net.createServer(function(socket) {
+  socket.on('data', function(data) {
+    console.log(data.toString('utf8'));
+
+    socket.write(data);
+  });
+}).listen(52273, function() {
   console.log('server on');
 });
