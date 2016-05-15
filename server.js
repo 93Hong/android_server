d904461f8b763c30424568f37011c0485d33e7c2
93Hong <sk2rldnr@gmail.com>
2016-05-16 01:27:31 +0900

    set and get childs location donegit add .git add .git add .git add .git add .!

--- server.js
+++ server.js
@@ -12,12 +12,6 @@ db.on("error", function(err) {
     console.log("DB error : ", err);
 });
 
-var c = mongoose.Schema({
-    username: String,
-    email: String
-});
-var C = mongoose.model('c', c);
-
 var childSchema = mongoose.Schema({
     username: {
         type: String,
@@ -33,10 +27,14 @@ var childSchema = mongoose.Schema({
         required: true,
         unique: true
     },
-    location: {
-        latitude: String,
-        longitude: String
-    },
+    location: [{
+        latitude: Number,
+        longitude: Number,
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }],
     parentName: {
         type: String,
         required: true
@@ -44,8 +42,7 @@ var childSchema = mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
-    },
-    count: Number
+    }
 });
 var Child = mongoose.model('child', childSchema);
 var parentSchema = mongoose.Schema({
@@ -70,17 +67,9 @@ var parentSchema = mongoose.Schema({
     childs: [{
         username: String,
         email: String
-    }],
-    numOfChild: {
-        type: Number,
-        default: 0
-    }
+    }]
 });
 var Parent = mongoose.model('parent', parentSchema);
-
-var clients = [];
-var count = 0;
-var messages = [];
 
 // show server ip address
 var interfaces = require('os').networkInterfaces();
@@ -91,10 +80,6 @@ for (var devName in interfaces) {
         if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
             console.log(alias.address);
     }
-}
-
-function create_id() {
-    return count += 1;
 }
 
 /*setInterval(function() { // send ping
@@ -144,6 +129,7 @@ var server = net.createServer(function(socket) {
         try {
             var packet = JSON.parse(data);
             var query;
+
             ///////////////////////////////////////////////////////////////////////////
             // LOGIN // REGISTER //
             ///////////////////////////////////////////////////////////////////////////
@@ -172,7 +158,6 @@ var server = net.createServer(function(socket) {
             }
 
             if (packet.type == "login") { // LOGIN
-
                 if (packet.userType == "Child")
                     query = getChildQuery(packet.username);
                 else if (packet.userType == "Parent")
@@ -193,17 +178,6 @@ var server = net.createServer(function(socket) {
             }
 
             if (packet.type == "register") { // REGISTER
-                /*var client = [];
-                client.socket = socket;
-                client.username = packet.username;
-                client.password = packet.password;
-                client.email = packet.email;
-                client.clientID = create_id();
-                //messages[packet.username] = [];
-                clients.push(client);
-                //var params = {};
-                //params.type = "register";
-                //params.clientID = client.clientID;*/
                 socket.write(JSON.stringify(packet.type + " " + packet.username) + "\n"); // [object Object]\n
 
                 ///////////////////////////  register in db
@@ -259,16 +233,21 @@ var server = net.createServer(function(socket) {
             // LOGIN // REGISTER // END
             ///////////////////////////////////////////////////////////////////////////
 
+            ///////////////////////////////////////////////////////////////////////////
+            // LIST // INSERT CHILDS //
+            ///////////////////////////////////////////////////////////////////////////
+
             if (packet.type == "list") {
                 getParentQuery(packet.username).exec(function(err, data) {
-                    if (data.childs.length > 0) {
-                        var rtn = "3-1";
+                    var rtn = "3-2";
+                    if (data.childs.length > 0) { // childs exist
+                        rtn = "3-1";
                         for (var i = 0; i < data.childs.length; i++)
                             rtn = rtn + data.childs[i].username + "/"; //3-1name/name/name
                         socket.write(rtn + '\n');
-                        console.log(rtn);
-                    }
-                    return;
+                    } else // childs not exist
+                        socket.write('3-2\n');
+                    return console.log(rtn);
                 });
             }
 
@@ -283,16 +262,66 @@ var server = net.createServer(function(socket) {
                         }
                     }
                 }, {
-                    upsert: true
+                    safe: true, upsert: true, new : true
                 }, function(err) {
                     if (err) {
                         console.log(err);
                     } else {
                         console.log("Successfully added");
+                        socket.write('Successfully added\n');
                     }
                 });
             }
+
             ///////////////////////////////////////////////////////////////////////////
+            // LIST // INSERT CHILDS // END
+            ///////////////////////////////////////////////////////////////////////////
+
+            ///////////////////////////////////////////////////////////////////////////
+            // GET CHILD LOCATION // SET CHILD LOCATION //
+            ///////////////////////////////////////////////////////////////////////////
+
+            if (packet.type == "getLocation") {
+                getChildQuery(packet.username).exec(function(err, data) {
+                    if (err) return console.log("Data error ", err);
+                    if (!data.location.length) {
+                      socket.write('getLocation error\n');
+                      return console.log('getLocation error');
+                    }
+                    var address = data.location.length - 1;
+                    var lat = data.location[address].latitude;
+                    var lng = data.location[address].longitude;
+                    console.log(data.username + '/' + lat + '/' + lng);
+                    socket.write(data.username + '/' + lat + '/' + lng + '\n'); // + query.createdAt
+                });
+            }
+
+            if (packet.type == "setLocation") {
+              Child.update({
+                  username: packet.username
+              }, {
+                  $push: {
+                      "location": {
+                          latitude: packet.lat,
+                          longitude: packet.lng
+                      }
+                  }
+              }, {
+                  safe: true, upsert: true, new : true
+              }, function(err) {
+                  if (err) {
+                      console.log(err);
+                  } else {
+                      console.log("Successfully added");
+                      socket.write('Successfully added\n');
+                  }
+              });
+            }
+
+            ///////////////////////////////////////////////////////////////////////////
+            // GET CHILD LOCATION // SET CHILD LOCATION // END
+            ///////////////////////////////////////////////////////////////////////////
+
         } catch (e) {
             console.log("Else " + e.message);
         }
